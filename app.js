0524d897ece89a727eb84d781d6fc4d5fafb38b3
bishal <parikhbishal@gmail.com>
2024-04-29 15:03:46 +0530

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('we have added new review stage'));
+app.get('/', (req, res) => res.send('We have added a  review stage with manual approval to our continous delivery pipeline'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
